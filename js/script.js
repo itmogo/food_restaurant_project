0487a22ba7f93a49826a947cc7dec3d8406e6ba0
itmogo <asamoah.frempong@gmail.com>
2022-07-11 15:37:15 +0000

    second commit

--- js/script.js
+++ js/script.js
@@ -35,3 +35,50 @@ document.querySelector('#search-icon').onclick = () =>{
 document.querySelector('#close').onclick = () =>{
     document.querySelector('#search-form').classList.remove('active');
    }
+
+var swiper = new Swiper(".home-slider", {
+    spaceBetween: 30,
+    centeredSlides: true,
+    autoplay:{
+        delay: 7500,
+        disableOnInteraction: false,
+    },
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+    loop: true,
+});
+
+var swiper = new Swiper(".review-slider", {
+    spaceBetween: 20,
+    centeredSlides: true,
+    autoplay: {
+        delay:7500,
+        disableOnInteraction: false,
+    },
+    loop: true,
+    breakpoints: {
+        0: {
+            slidePerView: 1,
+        },
+        640: {
+            slidePerView: 2,
+        },
+        768: {
+            slidePerView: 2,
+        },
+        1024: {
+            slidePerView: 3,
+        },
+    },
+});
+
+function loader(){
+    document.querySelector('.loader-container').classList.add('fade-out');
+ }
+
+ function fadeOut(){
+    setInterval(loader, 3000);
+ }
+ window.onload = fadeOut;
